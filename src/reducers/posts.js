8130131a510150d59f3cae97395da29ec79fa9ad
kocak-ilyas <62467305+kocak-ilyas@users.noreply.github.com>
2021-02-23 17:39:12 +0300

    eslint

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,4 +1,5 @@
 import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (posts = [], action) => {
   switch (action.type) {
     case FETCH_ALL : return action.payload;
