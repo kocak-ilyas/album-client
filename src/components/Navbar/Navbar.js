548e5840ae5ea1711820e7a3499a3fc8cd8af724
kocak-ilyas <62467305+kocak-ilyas@users.noreply.github.com>
2021-03-08 15:16:37 +0300

    Home and Sign button

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,63 @@
 import React from "react";
-import { AppBar, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import { AppBar, Avatar, Button, Toolbar, Typography } from "@material-ui/core";
 import useStyles from "./styles";
 import album from "../../images/album.png";
 
 const Navbar = () => {
   const classes = useStyles();
-
+  const user = null;
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
-      <Typography className={classes.heading} variant="h2" align="center">
-        Album
-      </Typography>
-      <img className={classes.image} src={album} alt="icon" height="60" />
+      <div className={classes.brandContainer}>
+        <Typography
+          component={Link}
+          to="/"
+          className={classes.heading}
+          variant="h2"
+          align="center"
+        >
+          Album
+        </Typography>
+        <img className={classes.image} src={album} alt="icon" height="60" />
+      </div>
+      <Toolbar className={classes.toolbar}>
+        {user ? (
+          <div className={classes.profile}>
+            <Avatar
+              className={classes.purple}
+              alt={user.result.name}
+              src={user.result.imageUrl}
+            >
+              {user.result.name.charAt(0)}
+            </Avatar>
+            <Typography
+              className={classes.userName}
+              variant="h6"
+              align="center"
+            >
+              {user.result.name}
+            </Typography>
+            <Button
+              className={classes.logout}
+              variant="contained"
+              color="secondary"
+            >
+              Logout
+            </Button>
+          </div>
+        ) : (
+          <Button
+            component={Link}
+            to="/auth"
+            variant="contained"
+            color="primary"
+            className={classes.logout}
+          >
+            Sign In
+          </Button>
+        )}
+      </Toolbar>
     </AppBar>
   );
 };
