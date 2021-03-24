cfe80691f071a6b9989c2c66350a144406963403
kocak-ilyas <62467305+kocak-ilyas@users.noreply.github.com>
2021-03-24 14:12:37 +0300

    netlify

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,13 @@
 import axios from "axios";
-const API = axios.create({ baseURL: "http://localhost:5000" }); // "https://album-server-project.herokuapp.com"
+// const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({baseURL: "https://album-server-project.herokuapp.com"});
+
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
     req.headers.Authorization = `Bearer ${
       JSON.parse(localStorage.getItem("profile")).token
     }`;
-  } 
+  }
   return req;
 });
 export const fetchPosts = () => API.get("/posts");
